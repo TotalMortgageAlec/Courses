80fe7336ed7374080d3a4cd809fa1c8ad1cb4ef2
TotalMortgageAlec <adasilva@totalmortgage.com>
2022-07-07 16:48:30 -0400

    Upload 7/7/2020

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -32,3 +32,43 @@ document.addEventListener("keydown", function (e) {
         closeModal();
     }
 });
+
+////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector(".header");
+const allSections = document.querySelectorAll(".section");
+console.log(allSections);
+
+document.getElementById("section--1");
+const allButtons = document.getElementsByTagName("button");
+console.log(allButtons);
+
+console.log(document.getElementsByClassName("btn"));
+
+// Creating and inserting elements
+// .insertAdjacentHTML              **USED IN SECTION 12: 2nd HALF OF BANKIST APP**
+const message = document.createElement("div");
+message.classList.add("cookie-message");
+// message.textContent = "We use cookies for improved functionality and analytics";
+message.innerHTML =
+    'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));      allows duplicates of a node instead of moving it
+
+// header.before(message);
+// header.after(message);
+
+// Delete elements
+document
+    .querySelector(".btn--close-cookie")
+    .addEventListener("click", function () {
+        message.remove();
+    });
