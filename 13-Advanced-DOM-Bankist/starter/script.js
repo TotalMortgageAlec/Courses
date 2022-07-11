e2fdb1b38ce0ca7b97ce5fff2b3737f8938e67b6
TotalMortgageAlec <adasilva@totalmortgage.com>
2022-07-11 13:11:51 -0400

    Dev Test

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -161,3 +161,17 @@ btnScrollTo.addEventListener('click', function (e) {
     // new technique, only works on modern browsers
     section1.scrollIntoView({ behavior: 'smooth' }); // modern method
 });
+
+const h1 = document.querySelector('h1');
+
+const alertH1 = function (e) {
+    alert('addEventListener: Great! You are reading the heading :D');
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000); // mouseenter event times out after 3 seconds
+
+// h1.onmouseenter = function (e) {
+//     alert('onmouseenter: Great! You are reading the heading :D'); // can call directly on event, but
+// }; // cannot add event listeners to same actions
