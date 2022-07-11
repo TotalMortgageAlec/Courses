4d1625ec4ad30e74afed01dbe14e3d5a0e737572
TotalMortgageAlec <adasilva@totalmortgage.com>
2022-07-11 09:50:35 -0400

    Test 4

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -1,34 +1,34 @@
-"use strict";
+'use strict';
 
 ///////////////////////////////////////
 // Modal window
 
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const btnCloseModal = document.querySelector(".btn--close-modal");
-const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
 const openModal = function (e) {
     e.preventDefault();
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
 };
 
 const closeModal = function () {
-    modal.classList.add("hidden");
-    overlay.classList.add("hidden");
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
 };
 
-btnsOpenModal.forEach(btn => btn.addEventListener("click", openModal));
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 // for (let i = 0; i < btnsOpenModal.length; i++)               ^^ same result, less concise
 //     btnsOpenModal[i].addEventListener("click", openModal);
 
-btnCloseModal.addEventListener("click", closeModal);
-overlay.addEventListener("click", closeModal);
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
 
-document.addEventListener("keydown", function (e) {
-    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
         closeModal();
     }
 });
@@ -41,23 +41,23 @@ console.log(document.documentElement);
 console.log(document.head);
 console.log(document.body);
 
-const header = document.querySelector(".header");
-const allSections = document.querySelectorAll(".section");
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
 console.log(allSections);
 
-document.getElementById("section--1");
-const allButtons = document.getElementsByTagName("button");
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
 console.log(allButtons);
 
-console.log(document.getElementsByClassName("btn"));
+console.log(document.getElementsByClassName('btn'));
 
 // Creating and inserting elements
-// .insertAdjacentHTML              **USED IN SECTION 12: 2nd HALF OF BANKIST APP**
-const message = document.createElement("div");
-message.classList.add("cookie-message");
+// .insertAdjacentHTML              **USED IN SECTION 12: 2nd HALF OF BANKIST APP**  Creates elements
+const message = document.createElement('div');
+message.classList.add('cookie-message');
 // message.textContent = "We use cookies for improved functionality and analytics";
 message.innerHTML =
-    'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+    "We use cookies for improved functionality and analytics. <button class='btn btn--close-cookie'>Got it!</button>";
 
 // header.prepend(message);
 header.append(message);
@@ -68,7 +68,7 @@ header.append(message);
 
 // Delete elements
 document
-    .querySelector(".btn--close-cookie")
-    .addEventListener("click", function () {
+    .querySelector('.btn--close-cookie')
+    .addEventListener('click', function () {
         message.remove();
     });
