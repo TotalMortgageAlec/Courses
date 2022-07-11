28b84b2f85954abb68568d371bf64a247379fecd
TotalMortgageAlec <adasilva@totalmortgage.com>
2022-07-11 10:27:52 -0400

    7/11/2022

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -71,4 +71,53 @@ document
     .querySelector('.btn--close-cookie')
     .addEventListener('click', function () {
         message.remove();
+        // message.parentElement.removeChild(message);
     });
+
+// Styles
+
+message.style.backgroundColor = '#37383d';
+message.style.width = '103.3%';
+
+console.log(message.style.height);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height); //height is a computed style
+
+message.style.height =
+    Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.className);
+
+logo.alt = 'Beautiful, minimalist logo';
+
+// Non-standard
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+logo.setAttribute('company', 'Bankist');
+
+console.log(logo.src);
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// Data attributes
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('c', 'j');
+logo.classList.remove('c', 'j');
+logo.classList.toggle('c');
+logo.classList.contains('c'); // contains, not includes (arrays)
+
+// Don't use - overrides all existing classes and only put one class on any element
+// logo.className = 'jonas';
