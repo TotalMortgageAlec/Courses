63000e83fa0675f47dcf75d1efd1d3ade7ad12e8
TotalMortgageAlec <adasilva@totalmortgage.com>
2022-07-14 16:57:25 -0400

    Ajax-4

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -201,9 +201,11 @@ const getCountryData = function (country) {
 
 // getCountryData('australia');
 
+/*
+
 /////////////////////////////////////////
 /////////////////////////////////////////
-// Coding Challenge
+// Coding Challenge 1
 
 const whereAmI = function (lat, lng) {
     fetch(
@@ -235,3 +237,68 @@ const whereAmI = function (lat, lng) {
 whereAmI(52.508, 13.381);
 whereAmI(19.037, 72.873);
 whereAmI(-33.933, 18.474);
+
+
+console.log('Test start');
+setTimeout(() => console.log('0 sec timer'), 0);
+Promise.resolve('Resolved promise 1').then(res => console.log(res));
+
+Promise.resolve('Resolved promise 2').then(res => {
+    for (let i = 0; i < 10000; i++) {}
+    console.log(res);
+});
+console.log('Test end');
+
+*/
+
+const lotteryPromise = new Promise(function (resolve, reject) {
+    console.log('The lottery draw is happening!');
+    setTimeout(function () {
+        if (Math.random() >= 0.5) {
+            resolve('YOU WIN!!!');
+        } else {
+            reject(new Error('YOU LOST!!!'));
+        }
+    }, 2000);
+});
+
+lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+// Promisifying setTimeout
+const wait = function (seconds) {
+    return new Promise(function (resolve) {
+        setTimeout(resolve, seconds * 1000);
+    });
+};
+
+// Refactoring into arrow functions
+const wait2 = seconds => {
+    return new Promise(resolve => {
+        setTimeout(resolve, seconds * 1000);
+    });
+};
+
+// wait2(2)
+//     .then(() => {
+//         console.log('I waited for 2 seconds');
+//         return wait2(1);
+//     })
+//     .then(() => console.log('I waited for 1 second'));
+
+wait2(1)
+    .then(() => {
+        console.log('1 second passed');
+        return wait2(1);
+    })
+    .then(() => {
+        console.log('2 seconds passed');
+        return wait2(1);
+    })
+    .then(() => {
+        console.log('3 seconds passed');
+        return wait2(1);
+    })
+    .then(() => console.log('I waited for 4 seconds'));
+
+Promise.resolve('abc').then(x => console.log(x));
+Promise.reject(new Error('Problem!')).catch(x => console.error(x));
