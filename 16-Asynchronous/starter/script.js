0eb94e0410c597e84904425469870a2fcec8c501
TotalMortgageAlec <adasilva@totalmortgage.com>
2022-07-18 12:12:21 -0400

    Ajax-7

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -509,5 +509,108 @@ get3Countries('portugal', 'france', 'italy');
 
 */
 
+/*
+
 ////////////////////////////////////////
 // Other Promise Combinators
+
+// Promise.race
+(async function () {
+    const [res] = await Promise.race([
+        getJSON(`https://restcountries.com/v2/name/italy`),
+        getJSON(`https://restcountries.com/v2/name/france`),
+        getJSON(`https://restcountries.com/v2/name/egypt`),
+    ]);
+    console.log(res);
+})();
+
+const timeout = function (sec) {
+    return new Promise(function (_, reject) {
+        setTimeout(function () {
+            reject(new Error('Request took too long!'));
+        }, sec * 1000);
+    });
+};
+
+Promise.race([
+    getJSON(`https://restcountries.com/v2/name/tanzania`),
+    timeout(0.1),
+])
+    .then(res => console.log(res[0]))
+    .catch(err => console.error(err));
+
+// Promise.allSettled
+// Returns an array of all settled promises
+// Doesn't fail if a single promise fails like .all()
+Promise.allSettled([
+    Promise.resolve('Success'),
+    Promise.reject('ERROR'),
+    Promise.resolve('Another success'),
+]).then(res => console.log(res));
+
+Promise.all([
+    Promise.resolve('Success'),
+    Promise.reject('ERROR'),
+    Promise.resolve('Another success'),
+]).then(res => console.log(res));
+
+// Promise.any [ES2021]
+// Returns the first resolved promise
+Promise.any([
+    Promise.resolve('Success'),
+    Promise.reject('ERROR'),
+    Promise.resolve('Another success'),
+]).then(res => console.log(res));
+
+*/
+
+//////////////////////////////////////////////////////
+// Coding Challenge 3
+
+const wait = seconds => {
+    return new Promise(resolve => {
+        setTimeout(resolve, seconds * 1000);
+    });
+};
+
+const imgContainer = document.querySelector('.images');
+
+const createImage = function (imgPath) {
+    return new Promise(function (resolve, reject) {
+        const img = document.createElement('img');
+        img.src = imgPath;
+
+        img.addEventListener('load', function () {
+            imgContainer.append(img);
+            resolve(img);
+        });
+
+        img.addEventListener('error', function () {
+            reject(new Error('Image not found'));
+        });
+    });
+};
+
+let currentImg;
+
+createImage('img/img-1.jpg')
+    .then(img => {
+        currentImg = img;
+        console.log('Image 1 loaded');
+        return wait(2);
+    })
+    .then(() => {
+        currentImg.style.display = 'none';
+        return createImage('img/img-2.jpg');
+    })
+    .then(img => {
+        currentImg = img;
+        console.log('Image 2 loaded');
+        return wait(2);
+    })
+    .then(() => {
+        currentImg.style.display = 'none';
+    })
+    .catch(err => console.error(err));
+
+const loadNPause = async function (imgArr) {};
